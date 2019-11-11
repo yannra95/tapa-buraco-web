bc7d13e4140f99f603a77f702f9732d989fa1249
Yann <yannra95@gmail.com>
2019-11-11 15:12:06 -0300

    Model: Definição dos modelos para Usuários, Agentes, e Buracos

--- tapaBuraco/buraco/models.py
+++ tapaBuraco/buraco/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	name = models.CharField(max_length=50)
+	holesDetected = models.IntegerField(max_length=20)
+	totalAvgAcc = models.FloatField()
+
+class Buraco(models.Model):
+	latitude = models.CharField(max_length=20)
+	longitude = models.CharField(max_length=20)
+	acc =  models.FloatField()
+	user = models.ManyToManyField(User)
+
+class Agent(models.Model):
+	name = models.CharField(max_length=50)
+	lastKnownLat = models.CharField(max_length=20)
+	lastKnownLon = models.CharField(max_length=20)
